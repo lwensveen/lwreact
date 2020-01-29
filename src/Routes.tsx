dfb31650089bf8514cf4128a389d81814de5aa91
Lodewijk Wensveen <lodewijkwensveen@gmail.com>
2020-01-29 16:22:20 +0100

    Added 404 and new route for detail view

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import asyncComponent from "./components/AsyncComponent";
+import NoMatch from "./components/404";
 
 const AsyncHome = asyncComponent(() => import("./containers/Home"));
 const AsyncWebShop = asyncComponent(() => import("./containers/webshop/WebShop"));
 const AsyncShop = asyncComponent(() => import("./containers/webshop/shop/Shop"));
+const AsyncShopDetail = asyncComponent(() => import("./containers/webshop/shop/detail/Detail"));
 const AsyncCSVUpload = asyncComponent(() => import("./containers/csv-upload/CSVUpload"));
 
 export default () =>
@@ -13,5 +15,7 @@ export default () =>
         <Route path="/" exact={true} component={AsyncHome}/>
         <Route path="/examples/webshop" exact={true} component={AsyncWebShop}/>
         <Route path="/examples/webshop/shop" exact={true} component={AsyncShop}/>
+        <Route path="/examples/webshop/shop/product/:title/:id" exact={true} component={AsyncShopDetail}/>
         <Route path="/examples/csv-upload" exact={true} component={AsyncCSVUpload}/>
+        <Route component={NoMatch}/>
     </Switch>;
