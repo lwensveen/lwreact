e2aa2bfc20feb9dfb8da7bd67720dcf4e357f43a
Lodewijk Wensveen <lodewijkwensveen@gmail.com>
2020-01-30 13:31:18 +0100

    Fixed typing

--- src/mocks/phones.ts
+++ src/mocks/phones.ts
@@ -1,19 +1,18 @@
 export interface Phone {
-    id: number;
-    title: string;
-    subtitle: string | null;
     brand: string;
-    os: string;
-    images: ImageSides;
-    colors: Colors[];
     bulletpoints: Bulletpoints[];
+    colors: Colors[];
     content: string;
+    id: number;
+    images: ImageSides;
+    model: string;
+    os: string;
 }
 
 export interface ImageSides {
+    back: string;
     front: string;
     side: string;
-    back: string;
 }
 
 export interface Bulletpoints {
@@ -23,100 +22,3 @@ export interface Bulletpoints {
 export interface Colors {
     color: string;
 }
-
-export const PHONES: Phone[] = [
-    {
-        id: 1,
-        title: "Samsung Galaxy S10",
-        subtitle: "128GB",
-        brand: "Samsung",
-        os: "Android",
-        images: {
-            front: "/webshop/dvi-front_Samsung_S10_Blue_305x450.png",
-            side: '',
-            back: '',
-        },
-        colors: [
-            {
-                color: "red",
-            },
-            {
-                color: "blue",
-            },
-        ],
-        bulletpoints: [
-            {
-                point: "Functional Programming",
-            },
-            {
-                point: "Prototypal Inheritance",
-            },
-            {
-                point: "Object composition > Classical inheritance",
-            },
-        ],
-        content: "Ik heb een ruime ervaring met Angular. ",
-    },
-    {
-        id: 2,
-        title: "Apple iPhone XS",
-        subtitle: "64GB",
-        brand: "Apple",
-        os: "iOS",
-        images: {
-            front: "/webshop/dvi-front_Samsung_S10_Blue_305x450.png",
-            side: '',
-            back: '',
-        }, colors: [
-            {
-                color: "red",
-            },
-            {
-                color: "blue",
-            },
-        ],
-        bulletpoints: [
-            {
-                point: "Functional Programming",
-            },
-            {
-                point: "Prototypal Inheritance",
-            },
-            {
-                point: "Object composition > Classical inheritance",
-            },
-        ],
-        content: "Ik heb een ruime ervaring met Angular. ",
-    },
-    {
-        id: 3,
-        title: "Samsung Galaxy S10",
-        subtitle: "128GB",
-        brand: "Samsung",
-        os: "Android",
-        images: {
-            front: "/webshop/dvi-front_Samsung_S10_Blue_305x450.png",
-            side: '',
-            back: '',
-        }, colors: [
-            {
-                color: "red",
-            },
-            {
-                color: "blue",
-            },
-        ],
-        bulletpoints: [
-            {
-                point: "Functional Programming",
-            },
-            {
-                point: "Prototypal Inheritance",
-            },
-            {
-                point: "Object composition > Classical inheritance",
-            },
-        ],
-        content: "Ik heb een ruime ervaring met Angular. ",
-    },
-];
