fe45b21b184b8b28bbde5c13bda496cac71954e3
Lodewijk Wensveen <lodewijkwensveen@gmail.com>
2020-02-04 14:47:02 +0100

    setup detail page

--- src/containers/webshop/shop/detail/Detail.tsx
+++ src/containers/webshop/shop/detail/Detail.tsx
@@ -1,13 +1,84 @@
-import React, { ReactElement } from "react";
-import { Box, Container, Toolbar } from "@material-ui/core";
+import React, { ReactElement, useEffect, useState } from "react";
+import { Box, Card, CircularProgress, Container, Radio, Toolbar } from "@material-ui/core";
+import { Phone as PhoneInterface } from "../../../../mocks/phones";
+import { useParams } from "react-router-dom";
+import { makeStyles } from "@material-ui/styles";
+import Steps from "../../../../components/webshop/steps/Steps";
+import Receipt from "../../../../components/webshop/receipt/Receipt";
+
+
+const useStyles = makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    },
+    toolbar: {
+        backgroundColor: '#fff',
+    }
+});
 
 export default function Detail(): ReactElement {
-    return (
-        <Container>
-            <Toolbar>Bla</Toolbar>
-            <Box display="flex" flexDirection="row">
-                <div>test</div>
+    const classes = useStyles();
+    const [phone, setPhone] = useState({} as PhoneInterface);
+    const [isLoaded, setLoaded] = useState(false);
+    const [error, setError] = useState();
+    const {id, title} = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/api/phones/${title}/${id}`)
+            .then((response) => response.json())
+            .then((getPhone: PhoneInterface) => {
+                setPhone({...getPhone});
+                setLoaded(true)
+            }, (error) => {
+                setError(error);
+                setLoaded(false)
+            });
+    }, [id, title]);
+
+    if (error) {
+        return (
+            <Box className={classes.root} display="flex" flexDirection="row" justifyContent="center"
+                 alignItems="center">
+                <div>Error: {error.message}</div>
             </Box>
-        </Container>
-    )
+        );
+    } else if (!isLoaded) {
+        return (
+            <Box className={classes.root} display="flex" flexDirection="row" justifyContent="center"
+                 alignItems="center">
+                <CircularProgress/>
+            </Box>
+        )
+    } else {
+        return (
+            <Box className={classes.root}>
+                <Container>
+                    <Box display="flex" flexDirection="row">
+
+                        <Box display="flex" flexDirection="column">
+                            <Toolbar className={classes.toolbar}>{phone.brand} {phone.model}</Toolbar>
+                            <Box display="flex" flexDirection="row">
+                                <Steps/>
+                            </Box>
+                        </Box>
+
+                        <Box display="flex" flexDirection="column">
+                            <img src={phone.images.front} alt={phone.brand + phone.model}/>
+
+                            <div>
+                                {/*<FlatButton><img src={phone.images.front} /></FlatButton>*/}
+                            </div>
+
+                            <Card>
+                                <Radio value="a"/>
+                                <Radio value="b"/>
+                            </Card>
+
+                            <Receipt/>
+                        </Box>
+                    </Box>
+                </Container>
+            </Box>
+        )
+    }
 }
