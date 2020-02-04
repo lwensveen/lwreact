43f1b7bc80d08bbd671bb6f9bb6c2a072907b5cd
Lodewijk Wensveen <lodewijkwensveen@gmail.com>
2020-02-04 14:48:30 +0100

    added types

--- src/serviceWorker.tsx
+++ src/serviceWorker.tsx
@@ -61,7 +61,7 @@ export function register(config?: Config) {
     }
 }
 
-function registerValidSW(swUrl: string, config?: Config) {
+function registerValidSW(swUrl: string, config?: Config): void {
     navigator.serviceWorker
         .register(swUrl)
         .then((registration) => {
@@ -105,7 +105,7 @@ function registerValidSW(swUrl: string, config?: Config) {
         });
 }
 
-function checkValidServiceWorker(swUrl: string, config?: Config) {
+function checkValidServiceWorker(swUrl: string, config?: Config): void {
     // Check if the service worker can be found. If it can't reload the page.
     fetch(swUrl)
         .then((response) => {
@@ -133,7 +133,7 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
         });
 }
 
-export function unregister() {
+export function unregister(): void {
     if ("serviceWorker" in navigator) {
         navigator.serviceWorker.ready.then((registration) => {
             registration.unregister();
